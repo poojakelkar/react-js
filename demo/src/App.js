65d61686c2a3b2c76da19667efd6925c338ea27e
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-23 20:31:36 +0530

    refactor

--- demo/src/App.js
+++ demo/src/App.js
@@ -9,7 +9,7 @@ constructor(){
 
 press()
 {
-    console.warn("submit")
+    alert("Form submit")
 }
 
 render() 
@@ -17,6 +17,8 @@ render()
     return(
         <div>
             <h1>Hello React</h1>
+            <input type="Email" placeholder="Enter Email ID"></input>
+            <input type="Password" placeholder="Enter Password"></input>
             <button onClick={()=>this.press()}>Press Me</button>
         </div>
     )
