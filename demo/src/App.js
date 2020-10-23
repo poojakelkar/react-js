c550091d64a3480bc38762be53c0ee0cfaf92574
Pooja Kelkar <poojakelkar53@gmail.com>
2020-10-23 20:37:14 +0530

    refactor

--- demo/src/App.js
+++ demo/src/App.js
@@ -17,8 +17,8 @@ render()
     return(
         <div>
             <h1>Hello React</h1>
-            <input type="Email" placeholder="Enter Email ID"></input>
-            <input type="Password" placeholder="Enter Password"></input>
+            <input type="Email" placeholder="Enter Email ID"></input><br></br>
+            <input type="Password" placeholder="Enter Password"></input><br></br><br></br>
             <button onClick={()=>this.press()}>Press Me</button>
         </div>
     )
