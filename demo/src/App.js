130345a7b81733e70570fef62bbfb446e3e3b434
Pooja Kelkar <poojakelkar53@gmail.com>
2020-11-04 18:45:52 +0530

    added file input

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 class App extends React.Component{
     render(){
         return(
-            <h1>Upload File in React js</h1>
+            <div>
+                <h1>Upload File in React js</h1>
+                <input type="file" onChange={(event)=>this.upload()} name="img"/>
+            </div>
         )
     }
 }
